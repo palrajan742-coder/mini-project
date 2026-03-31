b5a4fb0ccaed86aa1e493d24261da6e2b2bf643d
palrajan742-coder <palrajan742@gmail.com>
2026-03-31 09:35:51 -0700

    Add files via upload

---
+++ library management.py
@@ -0,0 +1,20 @@
+class Library:
+  def __init__(self):
+    self.noBooks = 0
+    self.books = []
+    
+  def addBook(self, book):
+    self.books.append(book)
+    self.noBooks = len(self.books)
+
+  def showInfo(self):
+    print(f"The library has {self.noBooks} books. The books are")
+    for book in self.books:
+      print(book)
+
+
+l1 = Library()
+l1.addBook("Harry Potter")
+l1.addBook("Seven Habits of Highly Effective People")
+l1.addBook("Think and Grow Rich")
+l1.showInfo()
